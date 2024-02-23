0bfc5c08261c1490d77dbe832f48d7e60180fd4f
opilane <opilane@HP-CZC2349HTB>
2024-02-23 08:55:36 +0200

    Add project files.

---
+++ l.py
@@ -0,0 +1,13 @@
+﻿From myModul import * #Summa
+ 
+#2
+while True:
+    try:
+        aasta=int(input("Sisesta aasta numer: "))
+        break
+    except:
+        print("viga")
+a=is_year_leap(aasta)
+print(a)
+
+
